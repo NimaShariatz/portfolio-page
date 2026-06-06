489ab5d8d33844546470cd6a0f948ab2e04b5221
NimaShariatz <shariatz@ualberta.ca>
2026-06-05 18:16:06 -0600

    setup work

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,19 @@
-
-
+import "./Home.css"
+import { Canvas } from '@react-three/fiber'
+import { Perf } from "r3f-perf"
 
 function Home() {
-    return(
-        <>
-        </>
-    )
+  return(
+    <>
+      <div className="canvas_container">
+      <Canvas>
+        <Perf position="top-left" />
+
+
+      </Canvas>
+      </div>
+    </>
+  )
 
 }
 
