f88013882f6bbc0cea9ff964983e4465d0dbf7d9
NimaShariatz <shariatz@ualberta.ca>
2026-06-07 03:16:41 -0600

    github pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-})
+
+  assetsInclude: ['**/*.glb'],
+
+    // Use the repo name for GitHub Pages build, but use '/' for local development
+  base: command === 'build' ? '/portfolio-page/' : '/',
+  build: {
+    outDir: 'dist'
+  }
+
+}))
