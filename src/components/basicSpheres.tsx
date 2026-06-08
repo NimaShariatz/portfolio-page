00ac233ee9380a819626ef9d6972203414008869
NimaShariatz <shariatz@ualberta.ca>
2026-06-08 03:37:11 -0600

    tweaks

--- src/components/basicSpheres.tsx
+++ src/components/basicSpheres.tsx
@@ -15,33 +15,33 @@ function BasicSpheres() {
     </mesh>
     
     <Float floatingRange={[0, 0.06]} rotationIntensity={0} speed={5}>
-      <mesh position={[9.5, 2.45, 9.5]}>
+      <mesh position={[9.5, 2.42, 9.5]}>
         <sphereGeometry args={[0.04, 10, 10]}/>
         <meshBasicMaterial color={"#c2262b"}/>
       </mesh>
 
-      <mesh position={[7.21, 1.53, 14.69]}>
+      <mesh position={[7.21, 1.54, 14.69]}>
         <sphereGeometry args={[0.04, 10, 10]}/>
         <meshBasicMaterial color={"#c7ad6b"}/>
       </mesh>
 
 
-      <mesh position={[7.27, 1.32, 14.91]}>
+      <mesh position={[7.27, 1.33, 14.91]}>
         <sphereGeometry args={[0.04, 10, 10]}/>
         <meshBasicMaterial color={"#c7ad6b"}/>
       </mesh>
 
-      <mesh position={[7.5, 1.32, 14.94]}>
+      <mesh position={[7.5, 1.33, 14.94]}>
         <sphereGeometry args={[0.04, 10, 10]}/>
         <meshBasicMaterial color={"#c7ad6b"}/>
       </mesh>
 
-      <mesh position={[7.54, 1.25, 15.13]}>
+      <mesh position={[7.54, 1.26, 15.13]}>
         <sphereGeometry args={[0.04, 10, 10]}/>
         <meshBasicMaterial color={"#c7ad6b"}/>
       </mesh>
 
-      <mesh position={[8.1, 1.32, 16.83]}>
+      <mesh position={[8.1, 1.33, 16.83]}>
         <sphereGeometry args={[0.04, 10, 10]}/>
         <meshBasicMaterial color={"#8BA046"}/>
       </mesh>
