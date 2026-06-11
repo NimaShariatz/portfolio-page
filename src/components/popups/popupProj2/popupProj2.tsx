7d036172dacb737062ea93c16c893f501d1cbe73
NimaShariatz <shariatz@ualberta.ca>
2026-06-11 00:45:03 -0600

    comments

--- src/components/popups/popupProj2/popupProj2.tsx
+++ src/components/popups/popupProj2/popupProj2.tsx
@@ -10,7 +10,7 @@ function PopupProj2({ triggerPopup, handle_triggerPopup }: PopupProj2Props) {
 
     <div className="proj2_container" style={{opacity: triggerPopup ? '1' : '0', pointerEvents: triggerPopup ? 'auto' : 'none'}} onClick={() => handle_triggerPopup('Proj2_popup')} aria-hidden={!triggerPopup}>
       <div className="proj2_content" style={{transform: triggerPopup ? 'translateY(-15px)' : 'translateY(0px)'}}>
-
+        <p style={{paddingLeft:"15%"}}> To be completed: June 14th</p>
       </div>
     </div>
 
